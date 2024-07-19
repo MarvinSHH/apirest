eae493ae8645250bf16e4eb3ed3fbcf604d88f9a
Marvin <marvinlaixs@gmail.com>
2024-07-19 02:43:08 -0600

    modified:   src/routes/repartidorAuth.routes.js

--- src/routes/repartidorAuth.routes.js
+++ src/routes/repartidorAuth.routes.js
@@ -1,10 +1,11 @@
 //src/routes/repartidorAuth.routes.js
 import { Router } from "express";
 import { methods as repartidorAuthController } from "../controller/repartidorAuth.controller";
+import { methods as recuperarContraenia } from "../controller/recuperacionContraenia.controller";
 
 const router = Router();
 
 router.post("/register", repartidorAuthController.registerRepartidor);
-router.post("/verificarCorreo", repartidorAuthController.recuperarContrasenia);
+router.post("/verificarCorreo", recuperarContraenia.verificarCorreo);
 
 export default router;
