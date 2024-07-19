13c83c1b503e153a28b7e53c6734f2b5abe47196
Marvin <marvinlaixs@gmail.com>
2024-07-19 11:17:36 -0600

    modified:   src/controller/repartidorAuth.controller.js

--- src/controller/repartidorAuth.controller.js
+++ src/controller/repartidorAuth.controller.js
@@ -3,30 +3,12 @@ import bcrypt from "bcryptjs";
 import { getConnection } from "../database/database";
 
 const registerRepartidor = async (req, res) => {
-  const {
-    nombre,
-    apaterno,
-    amaterno,
-    email,
-    contrasenia,
-    telefono,
-    pregunta_secreta,
-    respuesta_secreta,
-  } = req.body;
+  const { nombre, apaterno, amaterno, email, contrasenia, telefono } = req.body;
   try {
     const connection = await getConnection();
     const result = await connection.query(
-      "INSERT INTO tblrepartidor (nombre, apaterno, amaterno, email, contrasenia, telefono, pregunta_secreta, respuesta_secreta) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-      [
-        nombre,
-        apaterno,
-        amaterno,
-        email,
-        contrasenia,
-        telefono,
-        pregunta_secreta,
-        respuesta_secreta,
-      ]
+      "INSERT INTO tblrepartidor (nombre, apaterno, amaterno, email, contrasenia, telefono) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      [nombre, apaterno, amaterno, email, contrasenia, telefono]
     );
     res
       .status(201)
