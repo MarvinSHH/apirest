d1f379d6b239520630bc8f894184315acf5a5222
Marvin <marvinlaixs@gmail.com>
2024-07-25 09:15:44 -0600

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -9,5 +9,5 @@ export default {
   database: process.env.DB_NAME || "",
   user: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
-  port: process.env.DB_PORT || 3306, // Asegúrate de incluir el puerto
+  port: process.env.DB_PORT, // Asegúrate de incluir el puerto
 };
