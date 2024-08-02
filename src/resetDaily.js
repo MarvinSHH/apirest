a22b6f046a1434e01b7ee9ce101e7a86029f7a4e
Marvin <marvinlaixs@gmail.com>
2024-08-02 09:36:25 -0600

    modified:   src/resetDaily.js

--- src/resetDaily.js
+++ src/resetDaily.js
@@ -1,24 +1,3 @@
-/*const cron = require("node-cron");
-const { getConnection } = require("./database/database");
-
-// Programa la tarea para que se ejecute cada día a la medianoche
-cron.schedule("0 0 * * *", async () => {
-  try {
-    const connection = await getConnection();
-    const result = await connection.query(
-      `UPDATE tblclientesasignadosarepartidores 
-       SET visitado = 0, foto = NULL, historia = NULL`
-    );
-    console.log("Datos reiniciados diariamente:", result.affectedRows);
-  } catch (error) {
-    console.error("Error reiniciando los datos:", error);
-  }
-});
-
-console.log(
-  "Tarea programada para reiniciar datos diariamente a medianocheeee."
-);
-*/
 const cron = require("node-cron");
 const { getConnection } = require("./database/database");
 
