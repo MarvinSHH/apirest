bc65d9467e9567886b2836aca1d70302918fc8a0
Marvin <marvinlaixs@gmail.com>
2024-08-02 07:29:07 -0600

    modified:   src/controller/historial.controller.js

--- src/controller/historial.controller.js
+++ src/controller/historial.controller.js
@@ -6,7 +6,9 @@ const getHistorial = async (req, res) => {
   try {
     const connection = await getConnection();
     const result = await connection.query(
-      "SELECT idasignacion, idcliente, idrepartidor, visitado, foto, historia FROM tblclientesasignadosarepartidores"
+      `SELECT t1.idasignacion, t1.idcliente, t1.idrepartidor, t1.visitado, t1.foto, t1.historia, t2.nombre, t2.apaterno, t2.amaterno
+      FROM tblclientesasignadosarepartidores t1
+      JOIN tblcliente t2 ON t1.idcliente = t2.idcliente`
     );
     res.json(result);
   } catch (error) {
