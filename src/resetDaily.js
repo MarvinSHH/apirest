8916261b3464ce07ad4fc3432f747d6c69c5167f
Marvin <marvinlaixs@gmail.com>
2024-08-02 09:39:02 -0600

    modified:   src/resetDaily.js

--- src/resetDaily.js
+++ src/resetDaily.js
@@ -1,10 +1,11 @@
 const cron = require("node-cron");
 const { getConnection } = require("./database/database");
 
-// Programa la tarea para que se ejecute cada minuto (para pruebas)
 cron.schedule("* * * * *", async () => {
   try {
+    console.log("Intentando obtener la conexión a la base de datos...");
     const connection = await getConnection();
+    console.log("Conexión obtenida, intentando ejecutar la consulta...");
     const result = await connection.query(
       `UPDATE tblclientesasignadosarepartidores 
        SET visitado = 0, foto = NULL, historia = NULL`
