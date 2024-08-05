e8f2fef6c76422310b91bcdbd25c0c61bdc23aaa
Marvin <marvinlaixs@gmail.com>
2024-08-04 18:49:22 -0600

    modified:   src/controller/repartidorAuth.controller.js

--- src/controller/repartidorAuth.controller.js
+++ src/controller/repartidorAuth.controller.js
@@ -6,25 +6,10 @@ const registerRepartidor = async (req, res) => {
   const { nombre, apaterno, amaterno, email, contrasenia, telefono } = req.body;
   try {
     const connection = await getConnection();
-
-    // Verificar si el correo ya existe en tblrepartidor
-    const [existingRepartidor] = await connection.query(
-      "SELECT * FROM tblrepartidor WHERE email = ?",
-      [email]
-    );
-
-    if (existingRepartidor.length > 0) {
-      return res.status(400).json({ message: "El correo ya está registrado" });
-    }
-
-    // Hashear la contraseña antes de guardarla
-    const hashedPassword = await bcrypt.hash(contrasenia, 10);
-
     const result = await connection.query(
       "INSERT INTO tblrepartidor (nombre, apaterno, amaterno, email, contrasenia, telefono) VALUES (?, ?, ?, ?, ?, ?)",
-      [nombre, apaterno, amaterno, email, hashedPassword, telefono]
+      [nombre, apaterno, amaterno, email, contrasenia, telefono]
     );
-
     res
       .status(201)
       .json({ message: "Repartidor registrado exitosamente", result });
@@ -33,7 +18,6 @@ const registerRepartidor = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
 const recuperarContrasenia = async (req, res) => {
   const { email, preguntaSecreta, respuestaSecreta, nuevaContrasenia } =
     req.body;
